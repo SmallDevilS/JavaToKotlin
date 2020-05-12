19bf6a66c0433cedfc9daf2c49e79fb6c80957c8
bigdevil <bigdevil.cys@gmail.com>
2020-05-12 22:55:28 +0800

    init project

---
+++ base/src/main/java/com/example/core/BaseViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.core
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import java.util.*
+
+abstract class BaseViewHolder(itemView: View) : ViewHolder(itemView) {
+    @SuppressLint("UseSparseArrays")
+    private val viewHashMap: MutableMap<Int, View?> = HashMap()
+
+    protected fun <T : View> getView(@IdRes id: Int): T =
+        (viewHashMap[id] ?: itemView.findViewById<View>(id).also { viewHashMap[id] = it }) as T
+
+    protected fun setText(@IdRes id: Int, text: String?) {
+        getView<TextView>(id).text = text
+    }
+}
---
+++ lesson/src/main/java/com/example/lesson/LessonPresenter.kt
@@ -0,0 +1,31 @@
+package com.example.lesson
+
+import com.example.core.http.EntityCallback
+import com.example.core.http.HttpClient
+import com.example.core.utils.Utils
+import com.example.lesson.entity.Lesson
+import com.google.gson.reflect.TypeToken
+import java.util.*
+
+class LessonPresenter(private val activity: LessonActivity) {
+    companion object {
+        private const val LESSON_PATH = "lessons"
+    }
+
+    private var lessons: List<Lesson> = ArrayList()
+    private val type = object : TypeToken<List<Lesson?>?>() {}.type
+
+    fun fetchData() {
+        HttpClient.INSTANCE[LESSON_PATH, type, object : EntityCallback<List<Lesson>> {
+            override fun onSuccess(entity: List<Lesson>) {
+                this@LessonPresenter.lessons = entity
+                activity.runOnUiThread { activity.showResult(entity) }
+            }
+
+            override fun onFailure(message: String?) = activity.runOnUiThread { Utils.toast(message) }
+        }]
+    }
+
+    fun showPlayback() = activity.showResult(lessons.filter { it.state === Lesson.State.PLAYBACK })
+
+}
